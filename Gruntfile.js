b6a6aca2cf946e03e62ea0549ba0215b40cb558d
Nicole Krieger <ndkrieger@yahoo.com>
2016-03-25 23:19:49 -0400

    adding Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,6 +20,43 @@ module.exports = function(grunt) {
 		grunt.log.write('Hear hear, we are up...').ok();
 	});
 
+
+
+	/*
+	* Installing WP
+	*/
+	grunt.registerTask('wp-install', '', function() {
+		ls = getLocalsettings();
+		wpcmd = 'wp --path=' + ls.wppath + ' --allow-root ';
+
+		shell.mkdir('-p', ls.wppath);
+
+		if(!shell.test('-e', ls.wppath + '/wp-config.php')) {
+			shell.exec(wpcmd + 'core download --force');
+			shell.exec(wpcmd + 'core config --dbname=' + ls.dbname + ' --dbuser=' + ls.dbuser + ' --dbpass=' + ls.dbpass + ' --quiet');
+			shell.exec(wpcmd + 'core install --url=' + ls.url + ' --title="WordPress App" --admin_name=' + ls.wpuser + ' --admin_email="admin@local.dev" --admin_password="' + ls.wppass + '"');
+		} else {
+			grunt.log.write('Wordpress is already installed').ok();
+		}
+	});
+
+	/*
+	* Setting up WP, initiating it with content
+	* 
+	*/
+	grunt.registerTask('wp-setup', '', function() {
+		ls = getLocalsettings();
+		wpcmd = 'wp --path=' + ls.wppath + ' --allow-root ';
+		
+		pwd = shell.pwd();
+
+		// some standard plugins
+		shell.exec(wpcmd + 'plugin install --activate if-menu');
+		shell.exec(wpcmd + 'plugin install --activate baw-login-logout-menu');
+		shell.exec(wpcmd + 'plugin install --activate wp-cfm');
+
+	});
+
 	function getLocalsettings(test) {
 		var testMode = grunt.option('test');
 		if(test == true) {
@@ -35,4 +72,6 @@ module.exports = function(grunt) {
 		}
 		return ls;
 	}
+
+
 };
